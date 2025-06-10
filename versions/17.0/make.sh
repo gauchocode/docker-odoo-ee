7b8aaf4adc39a1710356c84ed09afd3037c50801
pjabie <pjabie@gauchocode.com>
2025-06-10 11:40:07 -0300

    Se agrega odoo enterprise v15

--- versions/17.0/make.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-sd build --rm=true -t gauchocode/odoo-gc:17.0 ./
-result=$?
-if [ "$result" -eq 0 ]; then
-    sd push gauchocode/odoo-gc:17.0
-else
-    echo "Falló la creación de la imagen"
-fi
-exit $return_code
